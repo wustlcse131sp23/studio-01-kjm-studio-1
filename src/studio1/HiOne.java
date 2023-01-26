7b81477bbe29e624fd1c7ffdb37cd8230c5e650d
kristinloftus <kristinloftus@localhost>
2023-01-26 15:22:55 -0600

    Finished studio 1

--- src/studio1/HiOne.java
+++ src/studio1/HiOne.java
@@ -10,14 +10,18 @@ public class HiOne {
 	public static void main(String[] args) {
 
 		Scanner in = new Scanner(System.in);
-		System.out.print("Enter value for arg 0: ");
-		String name = in.nextLine();
+		System.out.print("What is your name ");
+		String name1 = in.nextLine();
+	
+		System.out.println("hello " + name1);
 		//
 		// Below this line, enter code so that this program's output says
 		//      Hi, Pat.  How are you?
 		// if the value of name is "Pat"
 		//
 
+		
+		
 	}
 
 }
